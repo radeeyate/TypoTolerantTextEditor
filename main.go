01a6c9d7b65e7f23d4c6125bf0217341a3de53f0
Andrew <me@radi8.dev>
2024-08-14 12:36:44 -0600

    create big character map & add chances

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"math/rand"
-	//"time"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-
-	//"fyne.io/fyne/v2/driver/desktop"
-	"strings"
-
 	"fyne.io/fyne/v2/widget"
 )
+
+
 
 type entry1 struct {
 	widget.Entry
@@ -24,12 +22,6 @@ func newEntry1() *entry1 {
 	e.Entry.MultiLine = true
 	e.Entry.Wrapping = fyne.TextWrapWord
 	return e
-}
-
-func (e *entry1) TypedRune(r rune) {
-	//fmt.Printf("entry1 tr: %v\n", r)
-	e.Entry.TypedRune(r)
-	//printCursorPosition(e)
 }
 
 func (e *entry1) TypedKey(ke *fyne.KeyEvent) {
@@ -46,7 +38,7 @@ func printCursorPosition(e *entry1) {
 	row := e.Entry.CursorRow
 	text := e.Entry.Text
 	lineText := strings.Split(text, "\n")[row]
-	part1, part2 := text[:cursor], text[cursor:]
+	part1, part2 := lineText[:cursor], lineText[cursor:]
 	fmt.Println("part 1: " + part1)
 	fmt.Println("part 2: " + part2)
 	fmt.Println(cursor, row, lineText)
@@ -57,17 +49,34 @@ func printCursorPosition(e *entry1) {
 	typoWord := typoWord(modifyWord[0])
 	part1 += typoWord
 
-	fmt.Println(part1 + " " + part2)
+	newLines := strings.Split(text, "\n")
+	if len(part2) == 0 {
+		newLines[row] = part1
+	} else {
+		newLines[row] = part1 + " " + part2
+	}
+	newText := strings.Join(newLines, "\n")
 
-	e.Entry.SetText(part1 + " " + part2)
+	e.Entry.SetText(newText)
 }
 
 func typoWord(word string) string {
-	charset := "abcdefghijklmnopqrstuvwxyz"
 	newWord := ""
 	for i := 0; i < len(word); i++ {
-		if rand.Float32() < 0.3 {
-			newWord += string(charset[rand.Intn(len(charset))])
+		var chance float32
+		switch {
+		case len(word) < 5:
+			chance = 0.3
+		case len(word) > 5:
+			chance = 0.15 // make longer words have less chance of a typo
+
+		}
+		if rand.Float32() < chance {
+			if val, ok := keyboardMap[string(word[i])]; ok {
+				newWord += string(keyboardMap[string(word[i])][rand.Intn(len(val))])
+			} else {
+				newWord += string(word[i])
+			}
 		} else {
 			newWord += string(word[i])
 		}
@@ -81,7 +90,7 @@ func main() {
 	w := a.NewWindow("Abc")
 
 	editor1 := newEntry1()
-	editor1.SetPlaceHolder("Start typing #1 ok...")
+	editor1.SetPlaceHolder("Start typing here...")
 
 	content := container.NewStack(editor1)
 	w.SetContent(content)
